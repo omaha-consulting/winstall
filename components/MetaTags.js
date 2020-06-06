842205e49b979ff82f031e046ad1149f85a4ec89
MehediH <mehedi.dip@outlook.com>
2020-06-06 22:33:26 +0100

    Minor fixes

--- components/MetaTags.js
+++ components/MetaTags.js
@@ -13,6 +13,9 @@ const MetaTags = () => {
           content="Bulk install Windows apps quickly with Windows Package Manager."
         ></meta>
 
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#9b2eff" />
+        
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://winstall.app/" />
         <meta property="og:title" content="winstall" />
--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap"
