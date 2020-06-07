a8223ef43e82448f22938cbb20fd6553f324df74
MehediH <mehedi.dip@outlook.com>
2020-06-07 14:01:05 +0100

    Fix G

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
+        <Head/>
         <body>
           <Main />
           <NextScript />
@@ -17,17 +18,15 @@ class MyDocument extends Document {
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap"
           rel="stylesheet"
-        ></link>
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                <script>
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag() { dataLayer.push(arguments); }
-                  gtag('js', new Date());
+                window.dataLayer = window.dataLayer || [];
+                function gtag() { dataLayer.push(arguments); }
+                gtag('js', new Date());
 
-                  gtag('config', 'UA-48569869-4');
-                </script>
+                gtag('config', 'UA-48569869-4');
               `,
           }}
         />
