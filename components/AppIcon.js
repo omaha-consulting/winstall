beb006e22a6c747545f83f1317013dc2d0e3e726
Ladislau Szomoru <lszomoru@microsoft.com>
2020-06-24 22:21:53 +0200

    Pull request feedback

--- components/AppIcon.js
+++ components/AppIcon.js
@@ -3,6 +3,18 @@ import LazyLoad from "react-lazyload";
 import popularAppsList from "../data/popularApps.json";
 
 const AppIcon = ({id, name, icon}) => {
+    // if the app is listed in popularApps, use the image specified there
+    const popularApps = Object.values(popularAppsList).filter((app) => app._id === id);
+    if (popularApps.length === 1) {
+      return (
+        <AppPicture
+          name={name}
+          srcSetPng={`/assets/apps/fallback/${popularApps[0].img.replace("webp", "png")}`}
+          srcSetWebp={`/assets/apps/${popularApps[0].img}`}
+        />
+      );
+    }
+  
     if(!icon){ // if we don't have an icon, we mimmick one with app initials
         const nameParts = name.split(" ");
         let initials = name[0].substr(0, 1).toUpperCase();
@@ -35,39 +47,27 @@ const AppIcon = ({id, name, icon}) => {
       );
     }
 
-    // if the app is listed in popularApps, use the image specified there
-    const popularApps = Object.values(popularAppsList).filter((app) => app._id === id);
-    if (popularApps.length === 1) {
-      return (
-        <LazyLoad height={25} offset={300} once>
-          <picture>
-            <source srcSet={`/assets/apps/${popularApps[0].img}`} type="image/webp" />
-            <source srcSet={`/assets/apps/fallback/${popularApps[0].img.replace("webp", "png")}`} type="image/png" />
-            <img
-              src={`/assets/apps/fallback/${popularApps[0].img.replace("webp", "png")}`}
-              alt={`Logo for ${name}`}
-              draggable={false}
-            />
-          </picture>
-        </LazyLoad>
-      );
-    }
-
     icon = icon.replace(".png", "")
 
     return (
-      <LazyLoad height={25} offset={300} once>
-        <picture>
-          <source srcSet={`https://api.winstall.app/icons/next/${icon}.webp`} type="image/webp" />
-          <source srcSet={`https://api.winstall.app/icons/${icon}.png`} type="image/png" />
-          <img
-            src={`https://api.winstall.app/icons/${icon}.png`}
-            alt={`Logo for ${name}`}
-            draggable={false}
-          />
-        </picture>
-      </LazyLoad>
-    )
+      <AppPicture
+        name={name}
+        srcSetPng={`https://api.winstall.app/icons/${icon}.png`}
+        srcSetWebp={`https://api.winstall.app/icons/next/${icon}.webp`}
+      />
+    );
+}
+
+const AppPicture = ({ name, srcSetPng, srcSetWebp }) => {
+  return (
+    <LazyLoad height={25} offset={300} once>
+      <picture>
+        <source srcSet={srcSetWebp} type="image/webp" />
+        <source srcSet={srcSetPng} type="image/png" />
+        <img src={srcSetPng} alt={`Logo for ${name}`} draggable={false} />
+      </picture>
+    </LazyLoad>
+  );
 }
 
 export default AppIcon;
