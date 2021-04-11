3b9bf3f72874a1e24dd9ca99fc056a9de0305ef6
Mehedi <mehedi.dip@outlook.com>
2021-04-11 16:16:04 +0100

    Static support for /api/twitter

--- pages/api/twitter.js
+++ pages/api/twitter.js
@@ -1,9 +1,9 @@
-export default async function handler(req, res) {
+export const callTwitterAPI = async (endpoint, method="GET") => {
     let response, error;
 
     try {
-        const res = await fetch(req.headers.endpoint, {
-            method: req.method,
+        const res = await fetch(endpoint, {
+            method: method,
             headers: {
                 "Authorization": `Bearer ${process.env.NEXT_PUBLIC_TWITTER_BEARER}`
             }
@@ -17,6 +17,12 @@ export default async function handler(req, res) {
     } catch(err){
         error = err.message;
     }
+
+    return { response, error }
+}
+
+export default async function handler(req, res) {
+    const { response, error } = await callTwitterAPI(req.headers.endpoint, req.method);
 
     return res.send({ response, error });
 }
