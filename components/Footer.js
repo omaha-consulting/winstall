6fa56956bb4ef77d5f75e7cbc09c2e9e99e254c8
MehediH <mehedi.dip@outlook.com>
2022-01-12 23:22:57 +0000

    Vercel

--- components/Footer.js
+++ components/Footer.js
@@ -4,85 +4,110 @@ import Link from "next/link";
 import { FiChevronUp } from "react-icons/fi";
 import SelectedContext from "../ctx/SelectedContext";
 
-export default function Footer(){
+export default function Footer() {
   const { selectedApps, setSelectedApps } = useContext(SelectedContext);
 
   useEffect(() => {
-    window.onscroll = function(){scrollFunction()};
+    window.onscroll = function () {
+      scrollFunction();
+    };
   }, []);
 
   const scrollFunction = () => {
-    const btnTop = document.getElementById('btnTop');
-    
-    if((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && btnTop){
+    const btnTop = document.getElementById("btnTop");
+
+    if (
+      (document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20) &&
+      btnTop
+    ) {
       btnTop.style.display = "flex";
-    } else if(btnTop){
+    } else if (btnTop) {
       btnTop.style.display = "none";
     }
-  }
+  };
 
   const topFunction = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-  }
+  };
 
-    return (
-      <div className={styles.footer}>
-        <a
-          href="https://builtbymeh.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.brand}
-        >
-          <img
-            src={"/assets/meh.png"}
-            alt="builtbymeh.com logo"
-            draggable={false}
-          />
-          <p>Built by Mehedi Hassan.</p>
-        </a>
+  return (
+    <div className={styles.footer}>
+      <a
+        href="https://builtbymeh.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.brand}
+      >
+        <img
+          src={"/assets/meh.png"}
+          alt="builtbymeh.com logo"
+          draggable={false}
+        />
+        <p>Built by Mehedi Hassan.</p>
+      </a>
 
-        <ul>
-          <li>
-            <a
-              href="https://github.com/MehediH/winstall"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </li>
-          <li>
-            <Link href="/privacy">
-              <a>Privacy Policy</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/eli5">
-              <a>Help</a>
-            </Link>
-          </li>
-          <li>
-            <a
-              href="https://ko-fi.com/mehedi"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Donate
-            </a>
-          </li>
-        </ul>
+      <ul>
+        <li className="powered-by">
+          <a
+            href="https://vercel.com/?utm_source=builtbymeh&utm_campaign=oss"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by
+            <img
+              src="/vercel.svg"
+              className="sun"
+              alt="Powered by Vercel logo"
+            />
+            <img
+              src="/vercel-dark.svg"
+              className="moon"
+              alt="Powered by Vercel logo"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/MehediH/winstall"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <Link href="/privacy">
+            <a>Privacy Policy</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/eli5">
+            <a>Help</a>
+          </Link>
+        </li>
+        <li>
+          <a
+            href="https://ko-fi.com/mehedi"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Donate
+          </a>
+        </li>
+      </ul>
 
-        <span
-          id="btnTop"
-          title="Go to top"
-          onClick={topFunction}
-          className={`${styles.backToTop} ${
-            selectedApps.length !== 0 ? styles.selectionOpen : ""
-          }`}
-        >
-          <FiChevronUp />
-        </span>
-      </div>
-    );
+      <span
+        id="btnTop"
+        title="Go to top"
+        onClick={topFunction}
+        className={`${styles.backToTop} ${
+          selectedApps.length !== 0 ? styles.selectionOpen : ""
+        }`}
+      >
+        <FiChevronUp />
+      </span>
+    </div>
+  );
 }
