75c88b9d3bfd7958a82c29623f6183a514cdea54
MehediH <mehedi.hassan@circle.com>
2022-11-09 12:42:36 +0000

    fix next-pwa

--- pages/_document.js
+++ pages/_document.js
@@ -9,17 +9,21 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head/>
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
         <a id="gsc" href="#" download="winstall.bat" />
+        <link rel="manifest" href="/manifest.json" />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap"
           rel="stylesheet"
         />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-48569869-4"></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-48569869-4"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
