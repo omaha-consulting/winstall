8810c41cbbad87406f3a675eebbe3f80aaf21527
Michael Herrmann <michael@herrmann.io>
2024-03-15 06:29:46 +0100

    Point sponsored link at Pckgr

--- components/DonateCard.js
+++ components/DonateCard.js
@@ -5,11 +5,10 @@ import { RiPaypalFill } from "react-icons/ri";
 const DonateCard = ({ addMargin = "both" }) => {
     return (
         <div className={`${styles.container} ${addMargin === "both" ? styles.margin : (addMargin === "top" ? styles.marginTop : null)}`}>
-            <h2>Get your own winget repository</h2>
-            <p>This web site is brought to you by <a href="https://winget.pro">winget.Pro</a>. It's a service that gives you your own winget repository. Start uploading and installing your apps today!</p>
-
+            <h2>Seamless Intune Integration, Effortless App Management with Pckgr</h2>
+            <p><a href="https://intunepckgr.com/?navigation=winstall" rel="sponsored">Pckgr</a> simplifies your Intune application management. Utilizing winget, it streamlines software deployment with easy, one-click processes and automatic updates. Experience a user-friendly, secure way to manage apps across your enterprise. Start your 30 day free trial today!</p>
             <div className={styles.buttons}>
-                <a className="button spacer accent donate" id="starWine" href="https://winget.pro"><FiPlus /> Create a repository</a>
+                <a className="button spacer accent donate" id="starWine" href="https://intunepckgr.com/?navigation=winstall" rel="sponsored"><FiPlus /> Try for Free</a>
             </div>
         </div>
     )
